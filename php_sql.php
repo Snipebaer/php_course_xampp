17d39ece431a4bdcd1a771539f4d501992f335a6
Kilian <kilian.mader@hotmail.de>
2023-08-14 21:40:41 +0200

    add two ways to connect do db and select information

--- php_sql.php
+++ php_sql.php
@@ -1,15 +1,37 @@
 <?php
 
-echo "<pre>\n";
-
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc', 'root', 'root');
 
-$stmt = $pdo->query("SELECT * FROM users");
+// simply print data
 
-while($row = $stmt->fetch(PDO::FETCH_ASSOC))
+// echo "<pre>\n";
+
+// $stmt = $pdo->query("SELECT * FROM users");
+
+// while($row = $stmt->fetch(PDO::FETCH_ASSOC))
+// {
+//     print_r($row);
+// }
+
+// echo "</pre>\n";
+
+// Show data in form of a table
+
+$stmt = $pdo->query("SELECT name, email, password FROM users");
+
+echo '<table border="1">' . "\n";
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
 {
-    print_r($row);
+    echo "<tr><td>";
+    echo ($row['name']);
+    echo "</td><td>";
+    echo ($row['email']);
+    echo "</td><td>";
+    echo ($row['password']);
+    echo "</td></tr>\n";
 }
 
-echo "</pre>\n";
+echo "</table>\n";
+
 ?>
