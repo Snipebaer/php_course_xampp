7fdd4cf3eb93cdaa9689bd1cc2b7ff1d327a5745
Kilian <kilian.mader@hotmail.de>
2023-09-18 21:09:34 +0200

    add user2del.php file and add delete sql statement inside php tag

---
+++ inserts_sql_php/user2del.php
@@ -0,0 +1,15 @@
+<?php 
+
+require_once "..\pdo.php";
+
+if (isset($_POST['id']))
+{
+    $sql = "DELETE FROM users WHERE id = :zip";
+    
+    echo "<pre>\n$sql\n</pre>\n";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(':zip'=>$_POST['id']));
+}
+
+
+?>
