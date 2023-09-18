76253340575349ba3c2fc785102ca6ca0c6d9f21
Kilian <kilian.mader@hotmail.de>
2023-09-18 21:40:47 +0200

    add html code to show table which displays email, name, pwd and a delete button

--- inserts_sql_php/user3.php
+++ inserts_sql_php/user3.php
@@ -10,7 +10,37 @@ if (isset($_POST['delete']) && isset($_POST['id']))
     $stmt = $pdo->prepare($sql);
     $stmt->execute(array(':zip' => $_POST['id']));
 }
-
-
-
 ?>
+
+<html>
+    <head>
+        <title>Truly Wonderful the mind of a child is</title>
+    </head>
+
+    <body>
+        <table border="1">
+            <?php
+                $stmt = $pdo->query("SELECT name, email, password, id FROM users");
+
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+                {
+                    echo "<tr><td>";
+                    echo($row['name']);
+                    echo("</td><td>");
+                    echo($row['email']);
+                    echo("</td><td>");
+                    echo($row['password']);
+                    echo("</td><td>");
+                    echo('<form method="post"> <input type="hidden"');
+                    echo('name="id" value="'.$row['id'].'">'."\n");
+                    echo('<input type="submit" value="Del" name="delete">');
+                    echo("\n</form>\n");
+                    echo("</td></tr>");
+                }
+            ?>
+        </table>
+    </body>
+
+
+
+</html>
